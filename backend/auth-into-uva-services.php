aed7dd0d6c85cfe19c3ad884d66c5d7cad4cb871
Matt Quinn <mquinn13@stanford.edu>
2011-10-01 23:32:05 -0400

    Miscellaneous revisions.

--- backend/auth-into-uva-services.php
+++ backend/auth-into-uva-services.php
@@ -6,6 +6,8 @@
 	
 	$agent = new HTTPAgent();
 	
+	$REDIRECT_URL_TEMPLATE = 'https://shib0.itc.virginia.edu/shibboleth-idp/SSO?shire=https%3A%2F%2Fuofvirginia.netcardmanager.com%2FShibboleth.sso%2FSAML%2FPOST&time=&target=https%3A%2F%2Fuofvirginia.netcardmanager.com%2Fstudent%2Flocal_login.php&providerId=https%3A%2F%2Fuofvirginia.netcardmanager.com';
+	
 	function parse_redirect($response) {
 		$redirect_pos = strpos($response,'Location:');
 		if($redirect_pos!=false) {
@@ -19,24 +21,22 @@
 	}
 	
 	/*
-	* REQUEST #0 (GET)
+	* REQUEST #1
 	*/
 	$response = $agent->GET('https://uofvirginia.netcardmanager.com/student/local_login.php', true);
+	
 	$REDIRECT = parse_redirect($response);
-	
-	$template = 'https://shib0.itc.virginia.edu/shibboleth-idp/SSO?shire=https%3A%2F%2Fuofvirginia.netcardmanager.com%2FShibboleth.sso%2FSAML%2FPOST&time=&target=https%3A%2F%2Fuofvirginia.netcardmanager.com%2Fstudent%2Flocal_login.php&providerId=https%3A%2F%2Fuofvirginia.netcardmanager.com';
-	
 	$REDIRECT = substr($REDIRECT, strpos($REDIRECT, 'time'));
 	$REDIRECT = substr($REDIRECT, 0, strpos($REDIRECT, "&target"));
-	$REDIRECT = str_replace("time=", $REDIRECT, $template);
+	$REDIRECT = str_replace("time=", $REDIRECT, $REDIRECT_URL_TEMPLATE);
 	
 	/*
-	* REQUEST #1
+	* REQUEST #2
 	*/
 	$response = $agent->GET($REDIRECT, true);
 	
 	/*
-	* REQUEST #2
+	* REQUEST #3
 	*/
 	$response = $agent->GET('https://netbadge.virginia.edu/', false);
 	
@@ -46,13 +46,12 @@
 	$inputs = $xpath->evaluate('/html/body/div/div/div/fieldset/span/form[@action="index.cgi"]/input[@type="hidden"]');
 		
 	$formPostStr = 'user=mjq4aq&pass=<redacted>';
-		
 	for($i = 0; $i < $inputs->length; $i++) {
 		$formPostStr .= '&' . $inputs->item($i)->getAttribute('name') . '=' . $inputs->item($i)->getAttribute('value');
 	}
 	
 	/*
-	* REQUEST #3
+	* REQUEST #4
 	*/
 	$response = $agent->POST('https://netbadge.virginia.edu/index.cgi', $formPostStr, true);
 	
@@ -65,15 +64,12 @@
 	$REDIRECT = substr($response, strpos($response, "window.location.replace('"));
 	$REDIRECT = substr($REDIRECT, strpos($REDIRECT, "https://"));
 	$REDIRECT = substr($REDIRECT, 0, strpos($REDIRECT, "')\">"));
-	
-	$template = 'https://shib0.itc.virginia.edu/shibboleth-idp/SSO?shire=https%3A%2F%2Fuofvirginia.netcardmanager.com%2FShibboleth.sso%2FSAML%2FPOST&time=&target=https%3A%2F%2Fuofvirginia.netcardmanager.com%2Fstudent%2Flocal_login.php&providerId=https%3A%2F%2Fuofvirginia.netcardmanager.com';
-	
 	$REDIRECT = substr($REDIRECT, strpos($REDIRECT, 'time'));
 	$REDIRECT = substr($REDIRECT, 0, strpos($REDIRECT, "&amp;target"));
-	$REDIRECT = str_replace("time=", $REDIRECT, $template);
+	$REDIRECT = str_replace("time=", $REDIRECT, $REDIRECT_URL_TEMPLATE);
 	
 	/*
-	* REQUEST #4
+	* REQUEST #5
 	*/
 	$response = $agent->GET($REDIRECT, true);
 	
@@ -81,29 +77,28 @@
 	$dom->loadHTML($response);
 	$xpath = new DOMXPath($dom);
 	$inputs = $xpath->evaluate('//input[@type="hidden"]');
-	$formPostStr = '';
 
 	/*
 	 * MUST URL ENCODE THE POST PARAMTER VALUES OR SHIBBOLETH WONT BE ABLE TO DECODE THE SAMLRESPONSE PARAMETER
 	 */
+	$formPostStr = '';
 	for($i = 0; $i < $inputs->length; $i++) {
 		$formPostStr .= '&' . $inputs->item($i)->getAttribute('name') . '=' . urlencode($inputs->item($i)->getAttribute('value'));
 	}
 	$formPostStr = substr($formPostStr, 1);
 	
 	/*
-	* REQUEST #5
+	* REQUEST #6
 	*/
 	$response = $agent->POST('https://uofvirginia.netcardmanager.com/Shibboleth.sso/SAML/POST', $formPostStr, true);
-	$REDIRECT = parse_redirect($response);
 	
 	/*
-	* REQUEST #6
+	* REQUEST #7
 	*/
 	$response = $agent->GET('https://uofvirginia.netcardmanager.com/student/local_login.php', true);
 	
 	/*
-	* REQUEST #7
+	* REQUEST #8
 	*/
 	$response = $agent->GET('https://uofvirginia.netcardmanager.com/student/welcome.php', false);
 	
